f9570229c70061c72709500bde8f66dcf05b4755
chenxizhan <chenxizhan1995@163.com>
2018-06-14 19:14:48 +0800

    更改 ScoreDao 的方法名; 添加注释;
    modified:   FollowSystem/src/com/jxdedu/dao/ScoreDao.java

--- FollowSystem/src/com/jxdedu/dao/ScoreDao.java
+++ FollowSystem/src/com/jxdedu/dao/ScoreDao.java
@@ -11,10 +11,30 @@ import com.jxdedu.entity.Score;
  * @date 2018年6月13日
  */
 public interface ScoreDao {
-    boolean add(Score score);
-    boolean update(Score score);
+    /**
+     * 添加一条成绩记录.
+     * @param score 要添加的成绩对象
+     * @return  true,如果操作成功; false 如果操作失败
+     */
+    boolean addScore(Score score);
+    /**
+     * 更新成绩信息;
+     * 根据学生 ID 和课程 ID 更新其成绩.
+     * @param score 要更新的数据.
+     * @return  true,如果更新成功; false,如果更新失败;
+     */
+    boolean updateScore(Score score);
 
-    List<Score> getAll();
+    /**
+     * 获取所有成绩记录
+     * @return  由Score对象构成的列表; 其内包含数据库中所有成绩条目.
+     */
+    List<Score> getAllScore();
+    /**
+     * 查询记录总数.
+     * @return 数据库中,成绩记录总数.
+     */
     int getTotalCount();
     
 }
+
